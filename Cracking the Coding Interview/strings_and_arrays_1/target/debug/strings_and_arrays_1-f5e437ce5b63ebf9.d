e60b6383a9d544814e011ac720e5de2cef019daf
Sebastian <sebschwieger@gmail.com>
2020-04-27 22:37:26 +0200

    ...

--- Cracking the Coding Interview/strings_and_arrays_1/target/debug/strings_and_arrays_1-f5e437ce5b63ebf9.d
+++
@@ -1,1 +0,0 @@
-/home/sebastian/Documents/Rust-Projekte/Learning_Rust/Cracking\ the\ Coding\ Interview/strings_and_arrays_1/target/debug/strings_and_arrays_1-f5e437ce5b63ebf9: /home/sebastian/Documents/Rust-Projekte/Learning_Rust/Cracking\ the\ Coding\ Interview/strings_and_arrays_1/src/main.rs
--- Cracking the Coding Interview/strings_and_arrays_1/target/debug/strings_and_arrays_1.d
+++
@@ -1,1 +0,0 @@
-/home/sebastian/Documents/Rust-Projekte/Learning_Rust/Cracking\ the\ Coding\ Interview/strings_and_arrays_1/target/debug/strings_and_arrays_1: /home/sebastian/Documents/Rust-Projekte/Learning_Rust/Cracking\ the\ Coding\ Interview/strings_and_arrays_1/src/main.rs
