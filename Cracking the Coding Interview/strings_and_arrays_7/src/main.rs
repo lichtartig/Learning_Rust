02b23d3bfc4da0b254117b97c787cfa89059bffe
Sebastian <sebschwieger@gmail.com>
2020-04-28 20:49:53 +0200

    Implemented exercise no. 7

---
+++ Cracking the Coding Interview/strings_and_arrays_7/src/main.rs
@@ -0,0 +1,17 @@
+/// The object of this exercise is to rotate an NxN-matrix by 90°.
+/// The second question is if this can be done in place.
+extern crate ndarray;
+
+use ndarray::prelude::*;
+use ndarray::{Array, Ix2};
+
+/// Rotating a matrix 90° just means transposing it and then inverting the order of its rows.
+/// This is of course an operation in place, so we are done.
+fn main() {
+    // define an example matrix
+    let mut example: Array<i32, Ix2> = array![[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    // rotate it by 90°
+    example = example.t().to_owned();
+    example.invert_axis(Axis(1));
+    println!("{}", example);
+}
